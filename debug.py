08f121f92f5a09c23f3356a5d0c199b11cddeb2e
robertoaraujo347198 <98488389+robertoaraujo347198@users.noreply.github.com>
2022-02-02 23:33:51 -0500

    practica123
    inicio

---
+++ debug.py
@@ -0,0 +1,19 @@
+cadena = "Buenas días Profesor Ramiro Gustavo Banda Valdivia"
+
+print(cadena)
+
+lista_de_números = [20,30,40,50,60]
+
+print(lista_de_números)
+
+lista3 = [100,500,20,7000,-600]
+
+print(sum(lista3))
+print(max(lista3))
+print(min(lista3))
+
+
+
+
+
+
